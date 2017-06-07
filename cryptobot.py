a59de5ef3b3ab613b4c57fe0679d3bde0470acf5
Jesus Dominguez <jdominguez@talenssys.com>
2017-06-07 17:07:16 +0200

    Moved Telegram Bot logic to file telegramStuff.py. Might not have been a good move. We will see.

--- cryptobot.py
+++ cryptobot.py
@@ -10,34 +10,7 @@ __maintainer__ = "Angel Hernandez"
 __email__ = "angel@tupperbot.com"
 __status__ = "beta"
 
-from krakenStuff import *
-
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import logging
-import time
-
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# Define a few command handlers. These usually take the two arguments bot and
-# update. Error handlers also receive the raised TelegramError object in error.
-def start(bot, update):
-    update.message.reply_text("Hi! I'm Criptonito Bot")
-
-def help(bot, update):
-    update.message.reply_text('Help? I can barely help myself')
-
-def processMessage(bot, update):
-    print(update.message.text)
-    currency_pair, current_price, current_volume = getTiket(update.message.text)
-
-    update.message.reply_text("Pair: " + currency_pair +
-                            "\r\nEUR = " + str(current_price) +
-                            "\r\nVolume = " + str(current_volume))
-
-def error(bot, update, error):
-    logger.warn('Update "%s" caused error "%s"' % (update, error))
+from telegramStuff import *
 
 
 def main():
@@ -45,36 +18,8 @@ def main():
     print "|     Criptonito_bot 1.0      |"
     print "-------------------------------"
 
-    # Create the EventHandler and pass it your bot's token.
-    with open('telegramToken.txt', 'r') as f:
-        telegramToken = f.readline().strip()
-    updater = Updater(telegramToken)
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    #Send telegram message to Jesus
-    dp.bot.send_message(chat_id=42536066, text="CriptonitoBot Bot Just Started!")
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - Filter by Text (no photos or audios), and process the message
-    dp.add_handler(MessageHandler(Filters.text, processMessage))
-
-    # log all
-    dp.add_error_handler(error)
-
-    # Start the Bot
-    updater.start_polling()
-
-
-
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
+    robotito = TelegramBot()
+    
     try:
         while True:
             #Here we can do other stuff, like periodically check for prices in order to send alerts
@@ -82,8 +27,6 @@ def main():
     except KeyboardInterrupt:
         print("CriptonitoBot is sad and letting you go... :(")
         print("Press Ctrl-C once more to kill me")
-
-    updater.idle()
 
 
 if __name__ == '__main__':
---
+++ telegramStuff.py
@@ -0,0 +1,59 @@
+from krakenStuff import *
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+import time
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class TelegramBot:
+    def __init__(self):
+        # Create the EventHandler and pass it your bot's token.
+        with open('telegramToken.txt', 'r') as f:
+            self.telegramToken = f.readline().strip()
+        self.updater = Updater(self.telegramToken)
+
+        # Get the dispatcher to register handlers
+        self.dp = self.updater.dispatcher
+
+        #Send init telegram message to Jesus
+        self.dp.bot.send_message(chat_id=42536066, text="CriptonitoBot Bot Just Started!")
+
+        # on different commands - answer in Telegram
+        self.dp.add_handler(CommandHandler("start", self.start))
+        self.dp.add_handler(CommandHandler("help", self.help))
+
+        # on noncommand i.e message - Filter by Text (no photos or audios), and process the message
+        self.dp.add_handler(MessageHandler(Filters.text, self.processMessage))
+
+        # log all
+        self.dp.add_error_handler(self.error)
+
+        # Run the bot until you press Ctrl-C or the process receives SIGINT,
+        # SIGTERM or SIGABRT. This should be used most of the time, since
+        # start_polling() is non-blocking and will stop the bot gracefully.
+        self.updater.start_polling()
+
+        self.updater.idle()
+
+
+    # Define a few command handlers. These usually take the two arguments bot and
+    # update. Error handlers also receive the raised TelegramError object in error.
+    def start(self, bot, update):
+        update.message.reply_text("Hi! I'm Criptonito Bot")
+
+    def help(self, bot, update):
+        update.message.reply_text('Help? I can barely help myself')
+
+    def processMessage(self, bot, update):
+        print(update.message.text)
+        currency_pair, current_price, current_volume = getTiket(update.message.text)
+
+        update.message.reply_text("Pair: " + currency_pair +
+                                "\r\nEUR = " + str(current_price) +
+                                "\r\nVolume = " + str(current_volume))
+
+    def error(self, bot, update, error):
+        logger.warn('Update "%s" caused error "%s"' % (update, error))
